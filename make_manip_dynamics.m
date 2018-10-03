b174625c59a504663e49f66e6e832a910076b976
Ben Pski <ben@pepski.com>
2018-10-03 11:20:59 -0600

    Add linux comilation command.

--- make_manip_dynamics.m
+++ make_manip_dynamics.m
@@ -1,4 +1,10 @@
-%for windows
-setenv('MW_MINGW64_LOC','C:\TDM-GCC-64');
-mex -setup cpp
-mex -output manip_dynamics -I'C:\MinGW\msys\1.0\local\include\' matlab_interface.c dynamics.c 'C:\MinGW\msys\1.0\local\lib\libgsl.a' 'C:\MinGW\msys\1.0\local\lib\libgslcblas.a'
+if  isunix
+    mex GCC=/usr/bin/g++ -output manip_dynamics matlab_interface.c dynamics.C -lgsl -lgslcblas -lm
+elseif ispc
+    %for windows
+    setenv('MW_MINGW64_LOC','C:\TDM-GCC-64');
+    mex -setup cpp
+    mex -output manip_dynamics -I'C:\MinGW\msys\1.0\local\include\' matlab_interface.c dynamics.c 'C:\MinGW\msys\1.0\local\lib\libgsl.a' 'C:\MinGW\msys\1.0\local\lib\libgslcblas.a'
+else
+    error("Haven't tested compilation on this platform. Try something like:" + newline + "mex -output manip_dynamics matlab_interface.c dynamics.C -lgsl -lgslcblas -lm");
+end
