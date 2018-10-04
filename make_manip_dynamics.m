8f4a958384241ae0538bf536fc31b198e377526d
BenPski <ben@pepski.com>
2018-10-04 08:20:42 -0600

    Changed string formatting (has to be single quotes for older versions.

--- make_manip_dynamics.m
+++ make_manip_dynamics.m
@@ -6,5 +6,5 @@ elseif ispc
     mex -setup cpp
     mex -output manip_dynamics -I'C:\MinGW\msys\1.0\local\include\' matlab_interface.c dynamics.c 'C:\MinGW\msys\1.0\local\lib\libgsl.a' 'C:\MinGW\msys\1.0\local\lib\libgslcblas.a'
 else
-    error("Haven't tested compilation on this platform. Try something like:" + newline + "mex -output manip_dynamics matlab_interface.c dynamics.C -lgsl -lgslcblas -lm");
+    error(['Haven''t tested compilation on this platform. Try something like:', newline, 'mex -output manip_dynamics matlab_interface.c dynamics.C -lgsl -lgslcblas -lm']);
 end
